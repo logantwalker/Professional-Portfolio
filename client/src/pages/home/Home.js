fc8a362f8fe1aa87bc084bf070cbb06d68506fd2
L.Walker <logantwalker95@gmail.com>
2022-06-28 19:08:25 -0400

    changing video background from file to link

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -3,9 +3,6 @@ import { Parallax, Row, Col, Button, Carousel } from 'react-materialize';
 import Navbar from '../../components/Navbar/Navbar';
 import 'materialize-css';
 import './Home.css';
-
-import heroVid from './Assets/LOGANWALKERV1.mp4';
-
 import redSVG from './Assets/whiteLogo.svg';
 
 
@@ -16,7 +13,7 @@ export default function Home (props) {
             <div className='home-page'>
                 <div className='homepage-hero_video-container'>
                     <video muted playsInline autoPlay loop>
-                        <source src={heroVid} type='video/mp4' />
+                        <source src='https://i.imgur.com/fQNbEBE.mp4' type='video/mp4' />
                     </video>
                     <div className='homepage-hero_svg-container center'>
                         <img src={redSVG} className='svg-logo' alt='logo svg' />
